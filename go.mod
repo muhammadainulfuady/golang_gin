e6bf778351ad2d7f8cfddcefc4f74b7db94b874c
Muhammad Ainul Fuady <ainulfuadi1234@gmail.com>
2026-08-18 13:29:14 +0700

    gin uploads

--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/gofiber/utils/v2 v2.4.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/klauspost/compress v1.19.2 // indirect
 	github.com/klauspost/cpuid/v2 v2.4.0 // indirect
 	github.com/leodido/go-urn v1.5.0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
+)
+
+const (
+	MaxUploadSize = 1 << 20
 )
 
 type ApiResponse struct {
@@ -76,8 +82,87 @@ func main() {
 		})
 	})
 
+	router.POST("/upload", func(c *gin.Context) {
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"err": err.Error(),
+			})
+			return
+		}
+
+		// src, err := file.Open()
+		// if err != nil {
+		// 	c.JSON(http.StatusInternalServerError, gin.H{
+		// 		"err": "server err",
+		// 	})
+		// 	return
+		// }
+		// defer src.Close()
+
+		// buffer := make([]byte, 512)
+		// _, err = src.Read(buffer)
+		// if err != nil {
+		// 	c.JSON(http.StatusInternalServerError, gin.H{
+		// 		"error": "Gagal membaca isi file",
+		// 	})
+		// 	return
+		// }
+
+		// realContentType := http.DetectContentType(buffer)
+		// allowedType := map[string]bool{
+		// 	"image/jpeg": true,
+		// 	"image/jpg":  true,
+		// 	"image/png":  true,
+		// 	"image/webp": true,
+		// }
+
+		// if !allowedType[c.ContentType()] {
+		// 	c.JSON(http.StatusBadRequest, gin.H{
+		// 		"error": "File palsu terdeteksi! Tipe asli: " + realContentType,
+		// 	})
+		// 	return
+		// }
+		// c:/users/Asus/index.png
+
+		dst := filepath.Join("./uploadd/file", filepath.Base(file.Filename))
+		c.SaveUploadedFile(file, dst)
+		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+	})
+
+	router.MaxMultipartMemory = 1 << 20
+	router.POST("/uploads", uploadHandler)
+
 	err := router.Run(":3000")
 	if err != nil {
 		return
 	}
+}
+
+func uploadHandler(c *gin.Context) {
+	src, err := c.MultipartForm()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err":     err.Error(),
+			"message": "bad request pak",
+		})
+		return
+	}
+
+	files := src.File["file"]
+
+	for _, file := range files {
+		if file.Size > MaxUploadSize {
+			c.JSON(http.StatusRequestEntityTooLarge, gin.H{
+				"message": "filemu kegeden pak",
+			})
+			return
+		}
+		dst := filepath.Join("./uploads/files/", filepath.Base(filepath.Base(file.Filename)))
+		c.SaveUploadedFile(file, dst)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "upload successful",
+	})
 }
